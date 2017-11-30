b553c28bb80a6f4881451c854eeafdcd7d991c28
lastunicorn <coldblackstar@yahoo.com>
2017-11-30 09:41:56 +0200

    added the guardian classes

---
+++ source/ConsoleTools/Guard/GuardLevel.cs
@@ -0,0 +1,38 @@
+﻿// ConsoleTools
+// Copyright (C) 2017 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace DustInTheWind.ConsoleTools.Guard
+{
+    /// <summary>
+    /// Specifies the level at which an instance of the <see cref="Guardian"/> class will work.
+    /// </summary>
+    public enum GuardLevel
+    {
+        /// <summary>
+        /// The instance of the <see cref="Guardian"/> class will restrict the duplicates at the application level.
+        /// That means that two instances with the same name can exists in two different applications running
+        /// on the same machine.
+        /// </summary>
+        Application,
+
+        /// <summary>
+        /// The instance of the <see cref="Guardian"/> class will restrict the duplicates at the machine level.
+        /// That means that two instances with the same name are not allowed to exist in two different
+        /// applications running on the same machine. But in the same application they are allowed.
+        /// </summary>
+        Machine
+    }
+}
---
+++ source/ConsoleTools/Guard/MachineLevelGuardian.cs
@@ -0,0 +1,108 @@
+﻿// ConsoleTools
+// Copyright (C) 2017 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Threading;
+
+namespace DustInTheWind.ConsoleTools.Guard
+{
+    public class MachineLevelGuardian : IGuardian
+    {
+        /// <summary>
+        /// The <see cref="Mutex"/> object used to ensure that only one instance
+        /// of the class is created on the current machine. (Machine level)
+        /// </summary>
+        private Mutex mutex;
+
+        /// <summary>
+        /// Gets the name of the current instance.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MachineLevelGuardian"/> class with
+        /// the name that identifies it and 
+        /// the level at which will have efect.
+        /// </summary>
+        /// <param name="name">The name that identifies the instance that will be created.</param>
+        /// <param name="guardLevel">The level at which the new instance will have efect.</param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ApplicationException"></exception>
+        public MachineLevelGuardian(string name)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            Name = name;
+
+            // Create the mutex.
+            mutex = new Mutex(false, name);
+
+            // Gain exclusive access to the mutex.
+            bool access = mutex.WaitOne(0, true);
+
+            if (!access)
+                throw new ApplicationException(string.Format("Another instance with the name '{0}' already exists.", name));
+        }
+
+        private bool disposed;
+
+        /// <summary>
+        /// Releases all resources used by the current instance.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Releases all resources used by the current instance.
+        /// </summary>
+        /// <remarks>
+        /// <para>Dispose(bool disposing) executes in two distinct scenarios.</para>
+        /// <para>If the method has been called directly or indirectly by a user's code managed and unmanaged resources can be disposed.</para>
+        /// <para>If the method has been called by the runtime from inside the finalizer you should not reference other objects. Only unmanaged resources can be disposed.</para>
+        /// </remarks>
+        /// <param name="disposing">Specifies if the method has been called by a user's code (true) or by the runtime from inside the finalizer (false).</param>
+        private void Dispose(bool disposing)
+        {
+            // Check to see if Dispose has already been called.
+            if (!disposed)
+            {
+                // If disposing equals true, dispose all managed resources.
+                if (disposing)
+                {
+                    // Dispose managed resources.
+                    // ...
+
+                    mutex?.Close();
+                    mutex = null;
+                }
+
+                // Call the appropriate methods to clean up unmanaged resources here.
+                // ...
+
+                disposed = true;
+            }
+        }
+
+        ~MachineLevelGuardian()
+        {
+            Dispose(false);
+        }
+    }
+}

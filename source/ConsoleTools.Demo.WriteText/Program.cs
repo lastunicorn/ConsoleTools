11cd946be5facda8b15cd3784290056cd8c8834c
lastunicorn <coldblackstar@yahoo.com>
2017-12-27 17:03:20 +0200

    added a demo project for write text.

---
+++ source/ConsoleTools.Demo.WriteText/Program.cs
@@ -0,0 +1,56 @@
+﻿// ConsoleTools
+// Copyright (C) 2017 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+
+namespace DustInTheWind.ConsoleTools.Demo.WriteText
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            CustomConsole.WriteLineEmphasies("ConsoleTools Demo - Write Normal/Emphasized/Warning/Error");
+            CustomConsole.WriteLineEmphasies("===============================================================================");
+            CustomConsole.WriteLine();
+
+            try
+            {
+                CustomConsole.WriteLine("Normal: This is a normal line of text.");
+                CustomConsole.WriteLine();
+                CustomConsole.WriteLineEmphasies("Emphasies: But I can also write an emphasized text.");
+                CustomConsole.WriteLine();
+                CustomConsole.WriteLineSuccess("Success: And everything is ok if it finishes well :)");
+                CustomConsole.WriteLine();
+                CustomConsole.WriteLineWarning("Warning: But I have to warn you about the consequences of something not being done correctly.");
+                CustomConsole.WriteLine();
+                CustomConsole.WriteLineError("Error: If some error occures and the application will crush with an exception, I will display it on the screen immediately.");
+
+                throw new Exception("Some demo exception occured.");
+            }
+            catch (Exception ex)
+            {
+                CustomConsole.WriteLine();
+                CustomConsole.WriteLineError(ex);
+            }
+            finally
+            {
+                CustomConsole.WriteLine();
+                CustomConsole.WriteLine("Did you like our demo? :)");
+                CustomConsole.Pause();
+            }
+        }
+    }
+}
--- source/ConsoleTools.Demo.WriteText/Properties/AssemblyInfo.cs
+++ source/ConsoleTools.Demo.WriteText/Properties/AssemblyInfo.cs
@@ -15,24 +15,20 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyCompany("Dust in the Wind")]
-[assembly: AssemblyProduct("Console Tools")]
-[assembly: AssemblyCopyright("Copyright © Dust in the Wind 2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTitle("DustInTheWind.ConsoleTools.Demo.WriteText")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
-//[assembly: AssemblyFileVersion("1.0.0.*")]
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8dd165ba-78c3-48a0-bf61-9673458a260c")]

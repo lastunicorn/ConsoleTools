e78d0331b11c08e0d189aa8c4682c31ff1748f22
lastunicorn <coldblackstar@yahoo.com>
2018-10-05 19:22:29 +0300

    renamed some demo projects and their directories

---
+++ sources/ConsoleTools/ConsoleTools.Demo.InputControlsDemo/Program.cs
@@ -0,0 +1,79 @@
+﻿// ConsoleTools
+// Copyright (C) 2017-2018 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+
+namespace DustInTheWind.ConsoleTools.Demo.InputControlsDemo
+{
+    internal static class Program
+    {
+        private static ControlRepeater menuRepeater;
+
+        private static void Main()
+        {
+            Console.SetBufferSize(80, 1024);
+            Console.SetWindowSize(80, 60);
+
+            DisplayApplicationHeader();
+
+            menuRepeater = new ControlRepeater
+            {
+                Control = new MainMenu()
+            };
+
+            menuRepeater.Display();
+        }
+
+        private static void DisplayApplicationHeader()
+        {
+            //StackPanel stackPanel = new StackPanel
+            //{
+            //    Controls = new object[]
+            //    {
+            //        new InlineTextBlock
+            //        {
+            //            Text = "ConsoleTools Demo - InputControls",
+            //            ForegroundColor = CustomConsole.EmphasiesColor
+            //        },
+            //        new TextBlock
+            //        {
+            //            Text = CustomConsole.HorizontalWindowLine,
+            //            ForegroundColor = CustomConsole.EmphasiesColor,
+            //            MarginBottom = 1
+            //        },
+            //        new TextBlock
+            //        {
+            //            Text = "This demo shows the usage of the input controls (text and list).",
+            //            MarginBottom = 1
+            //        }
+            //    }
+            //};
+            //
+            //stackPanel.Display();
+
+            CustomConsole.WriteLineEmphasies("ConsoleTools Demo - InputControls");
+            CustomConsole.WriteLineEmphasies("===============================================================================");
+            CustomConsole.WriteLine();
+            CustomConsole.WriteLine("This demo shows the usage of the input controls (text and list).");
+            CustomConsole.WriteLine();
+        }
+
+        public static void Stop()
+        {
+            menuRepeater.RequestClose();
+        }
+    }
+}
---
+++ sources/ConsoleTools/ConsoleTools.Demo.TextMenuDemo/Program.cs
@@ -0,0 +1,94 @@
+﻿// ConsoleTools
+// Copyright (C) 2017-2018 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+
+namespace DustInTheWind.ConsoleTools.Demo.TextMenuDemo
+{
+    internal static class Program
+    {
+        private static GameApplication gameApplication;
+        private static ControlRepeater mainMenuRepeater;
+
+        private static void Main()
+        {
+            try
+            {
+                DisplayApplicationHeader();
+
+                Console.SetWindowSize(80, 50);
+                Console.SetBufferSize(80, 50);
+
+                Console.CancelKeyPress += HandleCancelKeyPress;
+
+                gameApplication = new GameApplication();
+
+                mainMenuRepeater = new ControlRepeater
+                {
+                    Control = new MainMenu(gameApplication)
+                };
+
+                gameApplication.Exited += HandleGameApplicationExited;
+
+                mainMenuRepeater.Display();
+
+                DisplayGoodby();
+            }
+            catch (Exception ex)
+            {
+                CustomConsole.WriteError(ex);
+            }
+            finally
+            {
+                Pause.QuickDisplay();
+            }
+        }
+
+        private static void HandleGameApplicationExited(object sender, EventArgs e)
+        {
+            mainMenuRepeater?.RequestClose();
+            gameApplication.Exited -= HandleGameApplicationExited;
+        }
+
+        private static void DisplayApplicationHeader()
+        {
+            CustomConsole.WriteLineEmphasies("ConsoleTools Demo - TextMenu");
+            CustomConsole.WriteLineEmphasies("===============================================================================");
+            CustomConsole.WriteLine();
+            CustomConsole.WriteLine("This demo shows how the TextMenu can be used.");
+            CustomConsole.WriteLine("Press the up/down arrow keys to navigate through the menu.");
+            CustomConsole.WriteLine("Press Enter key to select an item.");
+            CustomConsole.WriteLine();
+        }
+
+        private static void HandleCancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            gameApplication.RequestExit();
+        }
+
+        private static void DisplayGoodby()
+        {
+            TextBlock goodbyText = new TextBlock
+            {
+                Text = "Bye!",
+                ForegroundColor = CustomConsole.EmphasiesColor,
+                Margin = "0 1 0 0"
+            };
+            goodbyText.Display();
+        }
+    }
+}

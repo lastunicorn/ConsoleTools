687e09d5e86ba9e0e7ebb29b9d15bbde92e60f85
lastunicorn <coldblackstar@yahoo.com>
2021-12-14 08:04:40 +0200

    add drawio diagram

--- nuget2/pack.ps1
+++ nuget2/pack.ps1
@@ -1,32 +1,37 @@
 . .\config.ps1
 
-if (Test-Path "$packagesDirectory")
-{
+function PipePropertyTest {
+	param
+	(
+		[parameter(ValueFromPipelineByPropertyName)][int]$intP,
+		[parameter(ValueFromPipelineByPropertyName)][string]$stringP
+	)
+	Write-host "Int: $intP was recieved" -ForegroundColor Green
+	Write-host "String: $stringP was recieved" -ForegroundColor magenta
+}
+
+
+if (Test-Path "$packagesDirectory") {
 	Remove-Item "$packagesDirectory" -Recurse
 }
 
-foreach ($package in $packages)
-{
-	$nuspecExists = $package.NuspecPath -ne $null
+foreach ($package in $packages) {
+	$nuspecExists = $null -ne $package.NuspecPath
 	
-	if ($nuspecExists)
-	{
-		Write-Output "Nuspec file was specified. Try to generate package using the nuspec file."		
+	if ($nuspecExists) {
+		Write-Output "Nuspec file was provided. Try to generate package using the nuspec file."		
 		$nuspecFilePath = $package.NuspecPath
 		nuget pack "$nuspecFilePath" -OutputDirectory "$packagesDirectory" -Version "$version"
 	}
-	else
-	{
-		$projectExists = $package.ProjectPath -ne $null
+	else {
+		$projectExists = $null -ne $package.ProjectPath
 		
-		if ($projectExists)
-		{
+		if ($projectExists) {
 			Write-Output "Project file was specified. Try to generate package using the project file."
 			$projectPath = $package.ProjectPath
 			dotnet pack --configuration Release --output "$packagesDirectory" "$projectPath"
 		}
-		else
-		{
+		else {
 			$packageId = $package.PackageId
 			Write-Output "Package $packageId could not be generated. Nether nuspec or project files were provided."
 		}

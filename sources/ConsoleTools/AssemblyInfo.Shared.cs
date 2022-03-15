c88e0f9d271791e194ace2d7a4167aca4927e53f
lastunicorn <coldblackstar@yahoo.com>
2022-03-15 11:48:48 +0200

    update copright information

--- sources/ConsoleTools/AssemblyInfo.Shared.cs
+++ sources/ConsoleTools/AssemblyInfo.Shared.cs
@@ -1,5 +1,5 @@
 ﻿// ConsoleTools
-// Copyright (C) 2017-2020 Dust in the Wind
+// Copyright (C) 2017-2022 Dust in the Wind
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -26,7 +26,7 @@ using System.Reflection;
 // associated with an assembly.
 [assembly: AssemblyCompany("Dust in the Wind")]
 [assembly: AssemblyProduct("Console Tools")]
-[assembly: AssemblyCopyright("Copyright © Dust in the Wind 2017-2020")]
+[assembly: AssemblyCopyright("Copyright © 2017-2020 Dust in the Wind")]
 [assembly: AssemblyTrademark("")]
 
 // Version information for an assembly consists of the following four values:
